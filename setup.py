0e6e7d9c29e831bfa7b1dc64e3ca26c372262c04
Jay Morgan <jaymiles17@gmail.com>
2020-12-13 17:33:36 +0000

    Add MANIFEST and test build

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as file:
 
 setuptools.setup(
     name="labscribe",
-    version="0.3.0",
+    version="0.3.3",
     author="Jay Morgan",
     author_email="jay.p.morgan@outlook.com",
     description="A small package for managing python experiment scripts",
@@ -18,7 +18,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3",
     install_requires=required_packages,
-    package_data = {
-        "labscribe": ["labscribe/data/*.sql"],
-    },
+    include_package_data=True,
+    package_data={"labscribe": ["labscribe/data/*.sql"]},
 )
